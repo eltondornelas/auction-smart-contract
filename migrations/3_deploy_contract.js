fbfae92a080d0c35f785a2424ac115930a431b86
Elton Dornelas <eltondornelas@gmail.com>
2021-03-17 20:22:17 -0300

    adicionando outro address

--- migrations/3_deploy_contract.js
+++ migrations/3_deploy_contract.js
@@ -6,6 +6,7 @@ module.exports = function(deployer) {
     "Auction Porsche",
     1,
     20,
-    "0xd2c43daaccc1fcb2061d5a58099c1195e24f1f89"
+    "0x608AfE7bA0d3698683F952fd33621eF1BA08aA0D"
+    // "0xd2c43daaccc1fcb2061d5a58099c1195e24f1f89"
   );
 };
